ff7075fa7753e2a08ef90c7f077ca97aa0f8a116
simonebonifazi <simonbonifraz@gmail.com>
2022-07-11 18:45:47 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log(' vue ok')
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        firstName: 'Simone',
+        sourceImage: 'https://www.bing.com/images/search?view=detailV2&ccid=P7eNZbkF&id=EE243634565C8E1F867AAE4B66BB210E210E1677&thid=OIP.P7eNZbkFEgvW994ghATZyQHaFj&mediaurl=https%3a%2f%2fwww.icsedegliano.it%2fsezioni%2fstudenti%2f1112%2fimg%2fTragEdu%2f02SalutoInf2.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.3fb78d65b905120bd6f7de208404d9c9%3frik%3ddxYOIQ4hu2ZLrg%26pid%3dImgRaw%26r%3d0&exph=675&expw=900&q=immagini+saluto&simid=607996111981798275&FORM=IRPRST&ck=E593C87041856CA16D010D9F4235B689&selectedIndex=0',
+    },
+    methods: {
+
+    }
+})
